485c2c1062dc5977c2901693df18f387180e5901
itsJustLouis <l.k.monawe@gmail.com>
2024-11-07 11:08:00 +0200

    Re-uploaded videos

--- src/components/DesignComponent.jsx
+++ src/components/DesignComponent.jsx
@@ -1,38 +1,87 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import { motion, useAnimation } from "framer-motion";
 import "../css/design.scss";
 
-// Google Drive direct link format
+import wireframe01 from "../videos/Recipe.mp4";
+import wireframe02 from "../videos/Routes.mp4";
+// import wireframe03 from "../images/PCEssayPage.png";
+// import wireframe04 from "../images/MobileEssayPage.png";
+// import wireframe05 from "../images/PCDesignPage01.png";
+// import wireframe06 from "../images/PCDesignPage02.png";
+// import wireframe07 from "../images/profilePC.png";
+// import wireframe08 from "../images/profileMobile.png";
+// import wireframe09 from "../images/errorPage.png";
+// import wireframe10 from "../images/artpagePC.png";
+// import wireframe11 from "../images/artpageMobile.png";
+// import wireframe12 from "../images/revisedHomepage.png";
+import RecipeBook from "./recipeVideo";
+// wireframes for website
+
 const wireframes = [
   {
-    src:
-      "https://drive.google.com/uc?export=download&id=1jicniNl8GZCwjAaQCmIbzVYF4VeKvklC",
+    src: wireframe01,
     description: "Food Recipe Web Application.",
   },
   {
-    src:
-      "https://drive.google.com/uc?export=download&id=17IxD_XhHY0mb2Y9HWJaP1pVEAOidUDNf",
+    src: wireframe02,
     description: "Bus ticket and Routes Booking Web Application.",
   },
+  // {
+  //   src: wireframe03,
+  //   description: "",
+  // },
+  // {
+  //   src: wireframe04,
+  //   description: "",
+  // },
+  // {
+  //   src: wireframe05,
+  //   description: "",
+  // },
+  // { src: wireframe06, description: "" },
+  // {
+  //   src: wireframe07,
+  //   description: "",
+  // },
+  // { src: wireframe08, description: "" },
+  // {
+  //   src: wireframe09,
+  //   description: "",
+  // },
+  // {
+  //   src: wireframe10,
+  //   description: "",
+  // },
+  // {
+  //   src: wireframe11,
+  //   description: "",
+  // },
+  // {
+  //   src: wireframe12,
+  //   description: "",
+  // },
 ];
-
 function DesignComponent() {
   const [selectedWireframe, setSelectedWireframe] = useState(null);
-  const controls = useAnimation();
+  const controls = useAnimation(); // Controls for animations
 
   useEffect(() => {
     const sequence = async () => {
       for (let i = 0; i < wireframes.length; i++) {
-        await controls.start({ scale: [0, 1], transition: { duration: 0.5 } });
+        await controls.start({ scale: [0, 1], transition: { duration: 0.5 } }); // Animation sequence to zoom in images sequentially
       }
     };
-    sequence();
-  }, [controls]);
 
+    sequence();
+  }, [controls]); // will Run effect only once when controls change
+
+  // Function to handle click on a wireframe
   const handleClick = (wireframe) => {
     setSelectedWireframe(wireframe);
   };
 
+  // Function to close the modal
   const handleClose = () => {
     setSelectedWireframe(null);
   };
@@ -45,13 +94,18 @@ function DesignComponent() {
           key={index}
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5, delay: index * 0.1 }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          onClick={() => handleClick(wireframe)}
+          transition={{ duration: 0.5, delay: index * 0.1 }} // Delay animation for each wireframe
+          whileHover={{ scale: 1.1 }} // Scale animation on hover
+          whileTap={{ scale: 0.9 }} // Scale animation on tap/click
+          onClick={() => handleClick(wireframe)} // Click handler to display wireframe and text
         >
-          <video src={wireframe.src} controls width="100%" />
-          <p>{wireframe.description}</p>
+          <video
+            src={wireframe.src}
+            alt={`Wireframe ${index + 1}`}
+            controls
+            width="100%" // Adjust width as needed
+          />
+          <p>{wireframe.description}</p> {/* Description */}
         </motion.section>
       ))}
 
@@ -69,7 +123,12 @@ function DesignComponent() {
             animate={{ scale: 1 }}
             transition={{ duration: 0.3 }}
           >
-            <video src={selectedWireframe.src} controls width="100%" />
+            <video
+              src={selectedWireframe.src}
+              alt="Selected wireframe"
+              controls
+              width="100%" // Adjust width as needed
+            />
             <p>{selectedWireframe.description}</p>
           </motion.section>
         </motion.section>
